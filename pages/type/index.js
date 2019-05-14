85fc4fed717b1163ab86ae20f0317624e880dc85
e <kai_zou@emake.cn>
2019-05-14 09:46:29 +0800

    zk

--- pages/type/index.js
+++ pages/type/index.js
@@ -15,15 +15,13 @@ Page({
       url: '../logs/logs'
     })
   },
-  onLoad: function() {
-    // this.getUserInfo();
-    console.log(app.globalData);
+  onLoad: function () {
     if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
       })
-    } else if (this.data.canIUse) {
+    } else if (this.data.canIUse){
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
@@ -47,10 +45,10 @@ Page({
   },
   getUserInfo: function(e) {
     console.log(e)
-    // app.globalData.userInfo = e.detail.userInfo
-    // this.setData({
-    //   userInfo: e.detail.userInfo,
-    //   hasUserInfo: true
-    // })
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
   }
 })
